85ed67f8afece1ffcb78d629bc5f7284c20b0e45
EduardoFrAlmeida <eduardofralmeida745@gmail.com>
2021-11-15 20:26:42 -0300

    first commit

---
+++ aula11/if.js
@@ -0,0 +1,6 @@
+var vel = 90
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 80) {
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Siga as leis de trânsito!')
---
+++ aula11/ifEElse.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Se você nasceu no ${pais}:`)
+if ( pais != 'Brasil') {
+    console.log('Você é estrangeiro!')
+}else {
+    console.log('Você é Brasileiro!')
+}
